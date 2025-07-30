d0d7de6169ab07a32a26d5071c9df651839f23ff
pedro-arp <pedroagtrodrigues@gmail.com>
2025-07-30 18:31:34 -0300

    video05: RequestMapping and Servlet Context Path

--- spring-studies/src/main/java/academy/devdojo/controllers/HelloController.java
+++ spring-studies/src/main/java/academy/devdojo/controllers/HelloController.java
@@ -1,9 +1,11 @@
 package academy.devdojo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("greetings")
 public class HelloController {
     @GetMapping(value = {"hi","hi/"})
     public String hi(){
